8aaa73615182ff53a2fb026dce192aa724166468
jdw <jdwjyl2007@ajou.ac.kr>
2026-01-06 13:36:43 +0900

    edit usage comment at main page

--- demo.py
+++ demo.py
@@ -18,12 +18,12 @@ import openai
 litellm.drop_params = True
 
 st.set_page_config(
-    page_title="Materials Synthesis Recipe Recommender",
+    page_title="Recipe Recommendations for Inorganic Solid-State Synthesis",
     page_icon=":microscope:",
     layout="wide",
 )
 
-st.title("Materials Synthesis Recipe Recommender")
+st.title("Recipe Recommendations for Inorganic Solid-State Synthesis")
 # Input fields
 st.sidebar.title("Input Parameters")
 
@@ -205,7 +205,32 @@ if clear_btn:
     st.rerun()
 
 if not generate_btn and not hasattr(st.session_state, "messages"):
-    st.write("This is a demo of the Materials Synthesis Recipe Recommender. Please enter the desired material properties and click on the 'Recommend' button to get a list of materials synthesis recipes that can be used to synthesize materials with the desired properties.")
+#    st.write("This is a demo of the Materials Synthesis Recipe Recommender. Please enter the desired material properties and click on the 'Recommend' button to get a list of materials synthesis recipes that can be used to synthesize materials with the desired properties.")
+    st.write("""
+
+1. Enter your personal ```OpenAI``` API key.                
+        We added the following model options to the recipe generator.
+   ```
+   model_options = [
+        "gpt-4.1-mini",
+        "gpt-4o-mini",
+        "gpt-5.2",
+        "gpt-5-mini",
+        "gpt-5",
+        "o3",
+        "o3-mini",
+        "o3-mini-low",
+        "o3-mini-high"
+    ]
+    ```
+    Make sure your API key has access to the model you want to use. 
+    
+3. Click the ```Update``` key button.
+4. Provide the information needed for prediction (e.g., materials, synthesis technique, application, etc.).
+5. (Optional) Adjust the number of data entries used for RAG, or upload additional reference papers (PDFs).
+6. Click ```Recommend``` button at the bottom left to generate a recipe
+7. Click ```Clear Conversation``` to start a new prediction, or continue with follow-up Q&A using the ```chat box``` below the generated response. 
+    """)
     st.stop()
 
 use_rag = top_k >= 1
